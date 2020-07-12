6c5320d5ab0c98ce6f089ca3345cdd3ded7339e3
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-07-11 20:01:23 -0400

    Done

--- posts-router.js
+++ posts-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Posts = require('./data/seeds/01-posts');
+const Posts = require('./data/db');
 
 router.get('/', (req, res) => {
     Posts.find(req.query)
@@ -15,17 +15,100 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
-    Posts.find(req.query)
-    .then(posts => {
-        res.status(200).json(posts);
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if(post) {
+        res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'Post not found'})
+        }
     })
     .catch(error => {
-        concole.log("Error getting posts", error);
+        concole.log("Error getting a post", error);
         res.status(500).json({
-            message: "Error Retrieving posts",
+            message: "Error Retrieving a post",
         });
     });
 });
+
+router.get('/:id/comments', (req, res) => {
+    Posts.findCommentById(req.params.id)
+    .then(post => {
+        if(post) {
+        res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'Post not found'})
+        }
+    })
+    .catch(error => {
+        concole.log("Error getting a post", error);
+        res.status(500).json({
+            message: "Error Retrieving a post",
+        });
+    });
+});
+
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+    .then(post => {
+        res.status(201).json(post);
+    })
+    .catch( error => {
+        console.log("New post error", error);
+        res.status(500).json({
+            message: 'Error adding a post'
+        });
+    });
+});
+
+router.post('/:id/comments', (req, res) => {
+    Posts.insertComment(req.body)
+    .then(post => {
+        res.status(201).json(post);
+    })
+    .catch( error => {
+        console.log("New post error", error);
+        res.status(500).json({
+            message: 'Error adding a post'
+        });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({ message: 'Post Deleted'});
+        } else {
+            res.status(404).json({ message: 'Could not find the post'});
+        }
+    })
+    .catch(error => {
+        console.log('Error deleting a post', error);
+        res.status(500).json({
+            message: 'Error removing the post'
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    Posts.update(req.params.id, changes)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'the postcould not be found'});
+        }
+    })
+    .catch(error => {
+        console.log('Error updating a post', error);
+        res.status(500).json({
+            message: 'Error updating the post',
+        });
+    });
+});
+
 
 module.exports = router;
